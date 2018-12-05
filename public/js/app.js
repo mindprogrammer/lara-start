f94a981016ee27bcb1bf3998e469c7a1868db34f
Khoi Nguyen <khoi.ntq@gmail.com>
2018-12-04 21:21:28 -0500

    Sending HTTP Request After 3s

--- public/js/app.js
+++ public/js/app.js
@@ -68095,7 +68095,12 @@ Object.defineProperty(__webpack_exports__, "__esModule", { value: true });
     }
   },
   created: function created() {
+    var _this2 = this;
+
     this.loadUsers();
+    setInterval(function () {
+      return _this2.loadUsers();
+    }, 3000);
   }
 });
 
