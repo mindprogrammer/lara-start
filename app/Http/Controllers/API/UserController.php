2d9bb27d43ba5691559e13fb3cb095fae1ddedba
Khoi Nguyen <khoi.ntq@gmail.com>
2018-12-10 22:47:38 -0500

    Uploaded and Converted Base64 String to Image

--- app/Http/Controllers/API/UserController.php
+++ app/Http/Controllers/API/UserController.php
@@ -57,7 +57,13 @@ class UserController extends Controller
     {
         $user = auth('api')->user();
 
-        return $request->photo;
+        if ($request->photo) {
+
+            $name = time() . '.' . explode('/', explode(':', substr($request->photo, 0, strpos($request->photo, ';')))[1])[1];
+
+            \Image::make($request->photo)->save(public_path('img/profile/') . $name);
+
+        }
         // return ['message' => "Success"];
     }
 
